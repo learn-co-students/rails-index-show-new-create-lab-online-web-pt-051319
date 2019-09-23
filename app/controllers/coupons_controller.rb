c435ac920a22f00ac427c0aa2e416c06476f4d63
Kelley Chaplain <kmchaplain@gmail.com>
2019-09-23 11:03:44 -0500

    Done.

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -6,12 +6,21 @@ class CouponsController < ApplicationController
   end
 
   def create
+    # raise params.inspect
+    coupon = Coupon.new
+    coupon.store = params[:coupon][:store]
+    coupon.coupon_code = params[:coupon][:coupon_code]
+    coupon.save
+    render "show"
   end
 
   def new
+    render "new"
   end
   
   def show
+    @coupon = Coupon.find(params[:id])
+    render "show"
   end  
   
 end  
